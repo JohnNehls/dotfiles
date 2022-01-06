3e5d668f028f66e553afaf505e0586a869296dc8
JohnNehls <nehls@email.arizona.edu>
2022-01-06 00:43:07 -0600

    vertico -> Ivy,ivy-rich,counsel. Add Helpful. Theme -> gruvbox.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,59 +15,100 @@
    (package-install 'use-package))
 
 (require 'use-package)
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure t) ; no need for :ensure t for each package.
 
 (use-package command-log-mode)
 
-(setq inhibit-startup-message t)          ; inhibit startup message
-(tool-bar-mode -1)                        ; remove toolbar
-(scroll-bar-mode -1)                      ; remove side scrollbar
-(tooltip-mode -1)                     ; Disable tooltips
-;(menu-bar-mode -1)                   ; Disable the menu bar
-(set-fringe-mode 10)                  ; Give some breathing room
-;(global-display-line-numbers-mode t) ; show line numbers (better)
-(global-visual-line-mode t)       ; removes coninuation arrow
-(add-hook 'text-mode-hook 'flyspell-mode)   ;enable spellcheck on text mode
-(setq make-backup-files nil)      ; stop creating backup~ files
-(setq auto-save-default nil)      ; stop creating #autosave# files
-(add-to-list 'auto-mode-alist '("\\.text\\'" . org-mode)) ; open texts in org-mode
+;; Automatically tangle our Emacs.org config file when we save it
+(defun efs/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/.dotfiles/emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
+
+(setq inhibit-startup-message t)           ; inhibit startup message
+(tool-bar-mode -1)                         ; remove toolbar
+(scroll-bar-mode -1)                       ; remove side scrollbar
+(tooltip-mode -1)                          ; Disable tooltips
+(set-fringe-mode 10)                       ; Give some breathing room
+(global-visual-line-mode t)                ; removes coninuation arrow
+(add-hook 'text-mode-hook 'flyspell-mode)  ; enable spellcheck on text mode
+(setq make-backup-files nil)               ; stop creating backup~ files
+(setq auto-save-default nil)               ; stop creating #autosave# files
+;(menu-bar-mode -1)                        ; Disable the menu bar
+
+
+;; Open text files in Org-Mode
+(add-to-list 'auto-mode-alist '("\\.text\\'" . org-mode)) 
 (add-to-list 'auto-mode-alist '("\\.txt\\'" . org-mode))
 
 ;; Set up the visible bell
 (setq visible-bell t)
 
-;; Set up the visible bell
-(setq visible-bell t)
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-(use-package vertico
+;; ;Disable line numbers for some modes
+;;  (dolist (mode '(org-mode-hook
+;; 		 term-mode-hook
+;; 		 shell-mode-hook
+;; 		 eshell-mode-hook))
+;;    (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; (global-display-line-numbers-mode t)     ; Puts line numbers on ALL buffers
+
+(use-package gruvbox-theme
+    :init (load-theme 'gruvbox t))
+
+(defun transparency (value)
+   "Sets the transparency of the frame window. 0=transparent/100=opaque"
+   (interactive "nTransparency Value 0 - 100 opaque:")
+   (set-frame-parameter (selected-frame) 'alpha value))
+(transparency 96)
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package ivy
+  :diminish ivy-mode
+  :config
+  (ivy-mode 1))
+
+(use-package ivy-rich
   :init
-  (vertico-mode)
+  (ivy-rich-mode 1))
 
-  ;; Different scroll margin
-  ;; (setq vertico-scroll-margin 0)
-
-  ;; Show more candidates
-  ;; (setq vertico-count 20)
-
-  ;; Grow and shrink the Vertico minibuffer
-  ;; (setq vertico-resize t)
-
-  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-  ;; (setq vertico-cycle t)
-  )
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ;("C-r" . 'counsel-minibuffer-history)
+	 ))
 
 (use-package which-key
   :init (which-key-mode)
-  ;:diminish which-key-mode  ;remove name from minor mode list
+  :diminish which-key-mode  ;remove name from minor mode list
   :config
-  (setq which-key-idle-delay 1))
+  (setq which-key-idle-delay 1.0))
 
 (use-package neotree)
 
 (use-package magit)
 
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
 (use-package elpy
-  :ensure t
   :init (elpy-enable) ;enables Elpy in all future python buffers
   :custom
   (python-shell-interpreter "ipython")
@@ -105,27 +146,3 @@
 (add-to-list 'org-structure-template-alist '("sh" . "src sh"))
 (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
 (add-to-list 'org-structure-template-alist '("py" . "src python"))
-
-(use-package solarized-theme
-  :init (load-theme 'solarized-gruvbox-dark t))
-
-(require 'color)
-(set-face-attribute 'org-block nil :background
-                    (color-darken-name
-                     (face-attribute 'default :background) 3))
-
-(defun transparency (value)
-   "Sets the transparency of the frame window. 0=transparent/100=opaque"
-   (interactive "nTransparency Value 0 - 100 opaque:")
-   (set-frame-parameter (selected-frame) 'alpha value))
-(transparency 96)
-
-;; Automatically tangle our Emacs.org config file when we save it
-(defun efs/org-babel-tangle-config ()
-  (when (string-equal (buffer-file-name)
-                      (expand-file-name "~/.dotfiles/emacs.org"))
-    ;; Dynamic scoping to the rescue
-    (let ((org-confirm-babel-evaluate nil))
-      (org-babel-tangle))))
-
-(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
