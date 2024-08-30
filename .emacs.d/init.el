55a4bf9214d6e6808faee2970b43597c9a7fa0e4
JohnNehls <nehls@email.arizona.edu>
2024-08-30 19:13:59 -0400

    hackernews

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1451,3 +1451,20 @@ f"))
 
   (add-hook 'dashboard-mode-hook 'my-dashboard-hook)
   (add-hook 'dashboard-mode-hook 'hl-line-mode)
+
+(unless jmn-pure
+(use-package hnreader))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(gnus-secondary-select-methods 'nil)
+ '(gnus-select-method '(nnnil ""))
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
