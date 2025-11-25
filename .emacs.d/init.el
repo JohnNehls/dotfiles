00a27e0b3e5973fdf540c8cf1218465ad3ee84c6
JohnNehls <nehls@email.arizona.edu>
2025-11-24 19:01:36 -0500

    keep compilation buffers open even if they run successfully

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -753,23 +753,6 @@
 
 (setq compilation-scroll-output t) ;;*Compilation* buffer scroll with the output.
 
-(defun bury-compile-buffer-if-successful (buffer string)
- "Bury a compilation buffer if succeeded without warnings "
- (when (and
-         (buffer-live-p buffer)
-         (string-match "compilation" (buffer-name buffer))
-         (string-match "finished" string)
-         (not
-          (with-current-buffer buffer
-            (goto-char (point-min))
-            (search-forward "warning" nil t))))
-    (run-with-timer 1 nil
-                    (lambda (buf)
-                      (bury-buffer buf)
-                      (switch-to-prev-buffer (get-buffer-window buf) 'kill))
-                    buffer)))
-(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
-
 ;;settings
 (setq-default c-basic-offset 2)
 
